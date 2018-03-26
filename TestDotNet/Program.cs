1604241094c38ce2dd62015781d7c73f9c8a3c84
Yevgeniy.Pokotylo <ypokotylo@deloitte.com>
2018-03-27 00:11:37 +0300

    initial

---
+++ TestDotNet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using logic;
+
+namespace TestDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name!");
+            var name = Console.ReadLine();
+            var message = HelloWorld.GetMessage(name);
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using logic;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var expectedMessage = "Hello Immo!";
+            var actualMessage = HelloWorld.GetMessage("Immo");
+            Assert.Equal(expectedMessage, actualMessage);
+        }
+    }
+}
